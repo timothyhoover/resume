32893683b39339f6c8f710bb7928dacd2dfdd5a0
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2022-09-25 12:34:39 -0500

    add github link

--- components/Contact.vue
+++ components/Contact.vue
@@ -4,5 +4,5 @@
 div(class="flex flex-col items-center")
   h2(class="text-center") Get in Touch
   p(class="text-center mt-2 mb-8 text-xl font-light") Always open to discuss cool new opportunities!
-  button(class="py-4 px-6 w-2/4 md:w-1/4 bg-primary-blue text-white rounded-md text-xl font-medium") Say Hello
+  a(href="mailto:timmchoover@gmail.com" class="py-4 px-6 w-2/4 md:w-1/4 bg-primary-blue text-white text-center rounded-md text-xl font-medium") Say Hello
 </template>
--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -2,7 +2,9 @@
 
 <template lang="pug">
 section
-  h2(class="mb-5") Portfolio
+  div(class="flex space-x-2 items-center justify-between text-xl mb-5")
+    h2 Portfolio
+    Link(link='https://github.com/timothyhoover?tab=repositories' link-text='Github')
   main(class='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5')
     Project(src='/public/img/porter.png' link='https://www.orderwithporter.com')
     Project(src='/public/img/topic-talk.png' link='https://www.topictalkingles.com')
