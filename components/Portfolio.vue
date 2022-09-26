e81c456957f0110c9e79fb475beafb9c973c909b
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2022-09-25 19:08:10 -0500

    another img import fix attempt

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+import Porter from '@/assets/img/porter.png'
+import TopicTalk from '@/assets/img/topic-talk.png'
+import Blurock from '@/assets/img/blurock.png'
+import Workout from '@/assets/img/workout.png'
+import Evaly from '@/assets/img/evaly.png'
+import Reddit from '@/assets/img/reddit.png'
+</script>
 
 <template lang="pug">
 section
@@ -6,10 +13,10 @@ section
     h2 Portfolio
     Link(link='https://github.com/timothyhoover?tab=repositories' link-text='Github')
   main(class='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5')
-    Project(src='../assets/img/porter.png' link='https://www.orderwithporter.com')
-    Project(src='../assets/img/topic-talk.png' link='https://www.topictalkingles.com')
-    Project(src='../assets/img/blurock.png' link='https://www.blurockband.com')
-    Project(src='../assets/img/workout.png' link='https://workout-tracker-th.netlify.app')
-    Project(src='../assets/img/evaly.png' link='https://evaly-64cb8.web.app/')
-    Project(src='../assets/img/reddit.png' link='https://reddit-clone-wine.vercel.app/')
+    Project(:src='Porter' link='https://www.orderwithporter.com')
+    Project(:src='TopicTalk' link='https://www.topictalkingles.com')
+    Project(:src='Blurock' link='https://www.blurockband.com')
+    Project(:src='Workout' link='https://workout-tracker-th.netlify.app')
+    Project(:src='Evaly' link='https://evaly-64cb8.web.app/')
+    Project(:src='Reddit' link='https://reddit-clone-wine.vercel.app/')
 </template>
