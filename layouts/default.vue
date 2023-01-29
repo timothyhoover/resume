e68e263180e5a92faf24b7a008e5e7dda5544c8c
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2023-01-29 12:22:53 -0600

    add padding x back in

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,6 @@
 
 <template lang="pug">
 div(class='flex flex-col items-center h-screen')
-  div(class='py-16 max-w-[500px] md:max-w-screen-lg')
+  div(class='py-16 px-5 max-w-[500px] md:max-w-screen-lg')
     slot
 </template>
