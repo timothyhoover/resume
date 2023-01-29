a1129843960b62445697317af4aa309a1d931189
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2023-01-29 12:19:08 -0600

    another scroll fix attempt

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,7 @@
 <script setup></script>
 
 <template lang="pug">
-div(class='flex flex-col items-center h-screen overflow-x-hidden ')
-  div(class='flex-1 w-full py-16 px-5 overflow-y-scroll')
-    div(class="mx-auto max-w-[500px] md:max-w-screen-lg")
-      slot
+div(class='flex flex-col items-center h-screen')
+  div(class='py-16 max-w-[500px] md:max-w-screen-lg')
+    slot
 </template>
