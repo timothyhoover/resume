e54275320ecd6682b3790d7b6d06880dde4c4b2c
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2023-01-29 12:08:27 -0600

    attempt to fix weird scrolling behaviour on mobile devices

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
 
 <template lang="pug">
 div(class='flex flex-col items-center h-screen overflow-x-hidden ')
-  div(class='mx-auto flex-1 w-full max-w-[500px] md:max-w-screen-lg py-16 px-5')
-    slot
+  div(class='flex-1 w-full py-16 px-5 overflow-y-scroll')
+    div(class="mx-auto max-w-[500px] md:max-w-screen-lg")
+      slot
 </template>
