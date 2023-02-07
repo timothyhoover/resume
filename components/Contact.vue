382f388fc4465ecfeb8a3eac13201f0bd3c455a6
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2023-02-07 14:28:44 -0600

    update cv

--- components/Contact.vue
+++ components/Contact.vue
@@ -3,5 +3,5 @@ div(class="flex flex-col items-center")
   h2(class="text-center") Get in Touch
   p(class="text-center mt-2 mb-8 text-xl font-light") Always open to discuss cool new opportunities!
   a(href="mailto:timmchoover@gmail.com" class="mb-5 py-4 px-6 w-3/4 md:w-1/4 bg-primary-blue hover:opacity-90 text-white text-center rounded-md text-xl font-medium transition ease-in-out duration-75") #[span(class="mr-1") Say Hello] 👋🏻
-  a(href="https://drive.google.com/file/d/1vdzn55zY-2Y1iHxdDQc9Su-98KoQ6c4Q/view?usp=sharing" download target="_blank" class="py-4 px-6 w-3/4 md:w-1/4 border border-primary-blue hover:bg-primary-blue hover:bg-opacity-10 text-white text-center rounded-md text-xl font-medium transition-all ease-in-out duration-75") #[span(class="mr-1") Download CV] 📄
+  a(href="https://www.kickresume.com/cv/dPWb8/" download target="_blank" class="py-4 px-6 w-3/4 md:w-1/4 border border-primary-blue hover:bg-primary-blue hover:bg-opacity-10 text-white text-center rounded-md text-xl font-medium transition-all ease-in-out duration-75") #[span(class="mr-1") Download CV] 📄
 </template>
